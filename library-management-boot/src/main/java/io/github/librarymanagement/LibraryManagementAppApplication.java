e6603be2056ebb512df65db7f92ea88047552062
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2023-09-10 00:20:41 +0530

    Initial Commits for multi module project
    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- library-management-boot/src/main/java/io/github/librarymanagement/LibraryManagementAppApplication.java
+++ library-management-boot/src/main/java/io/github/librarymanagement/LibraryManagementAppApplication.java
@@ -3,7 +3,7 @@ package io.github.librarymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
+@SpringBootApplication
 public class LibraryManagementAppApplication {
 
 	public static void main(String[] args) {
