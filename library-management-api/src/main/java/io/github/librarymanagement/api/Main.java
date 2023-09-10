2138e71e9f7862a32c7108e502c042f3a8712d13
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2023-09-11 00:56:37 +0530

    initial commit files
    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- library-management-api/src/main/java/io/github/librarymanagement/api/Main.java
+++ library-management-api/src/main/java/io/github/librarymanagement/api/Main.java
@@ -1,4 +1,4 @@
-package io.github.librarymanagement;
+package io.github.librarymanagement.api;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
--- library-management-ms/src/main/java/io/github/librarymanagement/ms/Main.java
+++ library-management-ms/src/main/java/io/github/librarymanagement/ms/Main.java
@@ -1,4 +1,4 @@
-package io.github.librarymanagement;
+package io.github.librarymanagement.ms;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
